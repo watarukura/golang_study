5d2270cc5f8db86190687d306402c5defa452b7f
wataru-kurashima <wataru-kurashima@hands-lab.com>
2017-11-02 13:19:13 +0900

    curl option -d

--- curl/curl.go
+++ curl/curl.go
@@ -11,32 +11,38 @@ import (
 
 func main() {
 	var (
-		method string
-		url    string
-		head   string
+		method  string
+		url     string
+		head    string
+		payload string
 	)
 
 	flag.StringVar(&method, "X", "GET", "http method")
 	flag.StringVar(&head, "head", "", "http header")
 	flag.StringVar(&head, "H", "", "http header")
+	flag.StringVar(&payload, "d", "", "payload url-encode")
+	flag.StringVar(&payload, "data-urlencode", "", "payload url-encode")
 	flag.Parse();
 	url = flag.Args()[0]
 	heads := strings.Fields(head)
-	head_key := heads[0]
+	head_key := strings.Replace(heads[0], ":", "", -1)
 	head_val := heads[1]
 
 	req, err := http.NewRequest(method, url, nil)
 	if head != "" {
 		req.Header.Set(head_key, head_val)
 	}
+	if payload != "" {
+		req.PostFormValue(payload)
+	}
 	if err != nil {
-		fmt.Errorf("%s", err)
+		fmt.Println(req.Body)
+		fmt.Println(err)
 		os.Exit(1)
 	}
 
 	resp, err := http.DefaultClient.Do(req)
 	if err != nil {
-		fmt.Errorf("%s", err)
 		os.Exit(1)
 	}
 
@@ -44,7 +50,6 @@ func main() {
 
 	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
-		fmt.Errorf("%s", err)
 		os.Exit(1)
 	}
 	fmt.Println(string(body))
