925fff74d3e5829b1a6eeae5fba2d44fbca854c3
watarukura <what.r.j150@gmail.com>
2017-11-05 07:57:08 +0900

    scraper.go halfway

--- scraper/scraper/scraper.go
+++ scraper/scraper/scraper.go
@@ -2,8 +2,11 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
-	"os"
+	"net/http"
+	"log"
+
+	"golang.org/x/net/html"
+	"go/ast"
 )
 
 type Page struct {
@@ -11,10 +14,41 @@ type Page struct {
 	Description string `json:"description"`
 }
 
-func main() {
-	enc := json.NewEncoder(w)
-	if err := enc.Encode(p); err != nil {
-		fmt.Println(err)
-		os.Exit(1)
+func Get(url string) (*Page, error) {
+	resp, err := http.Get(url)
+	if err != nil {
+		return nil, err
 	}
+	defer resp.Body.Close()
+
+	doc, err := html.Parse(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	page := &Page{}
+	var f = func(node *html.Node)
+
+}
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		rawurl := r.FormValue("url")
+		if rawurl == "" {
+			http.Error(w, "url not specified", http.StatusBadRequest)
+			return
+		}
+		p, err := Get(rawurl)
+		if err != nil {
+			http.Error(w, "request failed", http.StatusInternalServerError)
+			return
+		}
+		w.Header().Set("Content-Type", "application/json")
+		enc := json.NewEncoder(w)
+		if err := enc.Encode(p); err != nil {
+			http.Error(w, "encoding failed", http.StatusInternalServerError)
+			return
+		}
+	})
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
