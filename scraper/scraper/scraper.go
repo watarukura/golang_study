23f2c43f557c926638693f511bfdc87ca4653f05
wataru-kurashima <wataru-kurashima@hands-lab.com>
2017-11-06 13:30:37 +0900

    multi url get halfway

--- scraper/scraper/scraper.go
+++ scraper/scraper/scraper.go
@@ -6,7 +6,6 @@ import (
 	"log"
 
 	"golang.org/x/net/html"
-	"go/ast"
 )
 
 type Page struct {
@@ -27,27 +26,58 @@ func Get(url string) (*Page, error) {
 	}
 
 	page := &Page{}
-	var f = func(node *html.Node)
+	var f func(node *html.Node)
+	f = func(n *html.Node) {
+		if n.Type == html.ElementNode && n.Data == "title" {
+			page.Title = n.FirstChild.Data
+		}
+		if n.Type == html.ElementNode && n.Data == "meta" {
+			if isDescription(n.Attr) {
+				for _, attr := range n.Attr {
+					if attr.Key == "content" {
+						page.Description = attr.Val
+					}
+				}
+			}
+		}
+		for c := n.FirstChild; c != nil; c = c.NextSibling {
+			f(c)
+		}
+	}
+	f(doc)
+	return page, nil
+}
 
+func isDescription(attrs []html.Attribute) bool {
+	for _, attr := range attrs {
+		if attr.Key == "name" && attr.Val == "description" {
+			return true
+		}
+	}
+	return false
 }
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		rawurl := r.FormValue("url")
-		if rawurl == "" {
-			http.Error(w, "url not specified", http.StatusBadRequest)
-			return
-		}
-		p, err := Get(rawurl)
-		if err != nil {
-			http.Error(w, "request failed", http.StatusInternalServerError)
-			return
-		}
-		w.Header().Set("Content-Type", "application/json")
-		enc := json.NewEncoder(w)
-		if err := enc.Encode(p); err != nil {
-			http.Error(w, "encoding failed", http.StatusInternalServerError)
-			return
+		r.ParseForm()
+		rawurls := r.Form["url[]"]
+		for i := 0; i < len(rawurls); i++ {
+			if rawurls[i] == "" {
+				http.Error(w, "url not specified", http.StatusBadRequest)
+				return
+			}
+			p, err := Get(rawurls[i])
+			if err != nil {
+				http.Error(w, "request failed", http.StatusInternalServerError)
+				return
+			}
+			w.Header().Set("Content-Type", "application/json")
+			enc := json.NewEncoder(w)
+			if err := enc.Encode(p); err != nil {
+				http.Error(w, "encoding failed", http.StatusInternalServerError)
+				return
+			}
+
 		}
 	})
 	log.Fatal(http.ListenAndServe(":8080", nil))
